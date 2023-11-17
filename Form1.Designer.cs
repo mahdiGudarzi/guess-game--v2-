207ab0296d2c0936f684110d77499fb23023d68e
mehdi <gudarzi.gi@gmail.com>
2023-11-17 09:54:17 +0330

    final

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace guess_game
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(165, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(314, 9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(469, 9);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(15, 162);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(165, 162);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(314, 162);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(469, 162);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(128, 118);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 118);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(165, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 118);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(314, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 118);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Location = new System.Drawing.Point(469, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 118);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label5.Location = new System.Drawing.Point(15, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 118);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label6.Location = new System.Drawing.Point(165, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 118);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label7.Location = new System.Drawing.Point(314, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 118);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label8.Location = new System.Drawing.Point(469, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 118);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 303);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "حدس تصویر";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Image = System.Drawing.Image;
+using Label = System.Windows.Forms.Label;
+namespace guess_game
+{
+
+   
+    public partial class form1 : Form
+    {
+        private string pic1,pic2 = default ;
+        Dictionary<string, Label> Pic = new Dictionary<string, Label>();
+        Label Cheker = default;
+        public form1()
+        {
+            InitializeComponent();
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectDirectory = Path.Combine(baseDirectory, "..", "..");
+            string imgFolderPath = Path.Combine(projectDirectory, "img");
+            string[] imageFiles = Directory.GetFiles(imgFolderPath, "*.jpg");
+            PictureBox[] pictureBoxes ={ pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8 };
+            Label[] labels ={label1,label2,label3,label4,label5,label6,label7,label8};
+            Random random = new Random();
+            foreach (PictureBox pictureBox in pictureBoxes)
+            {
+              
+                string randomImagePath = imageFiles[random.Next(imageFiles.Length)];
+                pictureBox.Image = Image.FromFile(randomImagePath);
+                pictureBox.Name = Path.GetFileNameWithoutExtension(randomImagePath);
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.SendToBack();
+
+
+            }
+            foreach (var label in labels)
+            {
+                label.Visible = true;
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox1.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox1.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox2.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox2.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox3.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox3.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox4.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox4.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox5.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox5.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox6.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox6.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox7.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox7.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+            if (Cheker != (sender as Label))
+            {
+                Console.WriteLine(sender);
+                if (string.IsNullOrEmpty(pic1))
+                {
+                    this.pic1 = this.pictureBox8.Name;
+                    Pic.Add("pic1", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+
+                }
+                else if (string.IsNullOrEmpty(pic2))
+                {
+                    this.pic2 = this.pictureBox8.Name;
+                    Pic.Add("pic2", (sender as Label));
+                    Cheker = (sender as Label);
+                    
+                }
+            }
+            check();
+
+
+        }
+
+        private void check()
+        {
+            if (!string.IsNullOrEmpty(pic1) && !string.IsNullOrEmpty(pic2))
+            {
+                if (string.Compare(pic1, pic2) == 0)
+                   
+                {
+                    Label temp;
+                    Pic.TryGetValue("pic1", out temp);
+                    temp.Visible = false;
+                    Pic.TryGetValue("pic2", out temp);
+                    temp.Visible = false;
+                }
+
+                this.pic1 = default;
+                this.pic2 = default;
+                this.Pic.Remove("pic1");
+                this.Pic.Remove("pic2");
+            }
+        }
+
+    }
+}
